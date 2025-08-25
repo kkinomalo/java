7a77f1a7bfd94cb834baddf5f5b4fee9f16b952f
nojiwan <nojiwan97@gmail.com>
2025-08-25 14:03:25 +0900

    First Commit4

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+/**
+ * @author 노지완
+ */
+public class Hello {
+//tjfauddlqslek gotjrdms dkfdktj dlrjteh dlfwhddml dkaghgkrdlfkrh gkf tn dLTrpTspdy
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+		
+		/*qjadnl
+		 * wntjr*/
+		
+	}
+
+}
