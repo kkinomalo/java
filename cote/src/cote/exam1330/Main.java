63204ef7560280b55592fded9d83c0e34c95b49c
Nohjiwan <nojiwan97@gmail.com>
2025-09-10 14:41:14 +0900

    Update Main.java

--- cote/src/cote/exam1330/Main.java
+++ cote/src/cote/exam1330/Main.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int year = sc.nextInt();
 
-        if (A > B) {
-            System.out.println(">");
-        } else if (A < B) {
-            System.out.println("<");
-        } else {
-            System.out.println("==");
+        if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println("1");
+        } 
+        else {
+        	System.out.println(0);
         }
     }
 }
