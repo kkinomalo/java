ffa3cb351cab75a45b835d0cbf6ad5c64b29a463
Nohjiwan <nojiwan97@gmail.com>
2025-09-22 15:56:47 +0900

    Update Car.java

--- chap06/src/sec06/exam06/Car.java
+++ chap06/src/sec06/exam06/Car.java
@@ -3,7 +3,29 @@ package sec06.exam06;
 public class Car {
     private int speed;
     private boolean stop;
-
+    
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+		 if(speed < 0) {
+	            this.speed = 0;
+	            return;
+	        } else {
+	            this.speed = speed;
+	        }
+	}
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		this.speed = 0;
+	}
+}
+    
+/*
     public int getSpeed() {
         return speed;
     }
@@ -24,3 +46,4 @@ public class Car {
         this.speed = 0;
     }
 }
+*/
