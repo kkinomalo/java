d71bb10cfb7478128ae9560c74c668846e98a342
Nohjiwan <nojiwan97@gmail.com>
2025-09-24 14:43:07 +0900

    Create D.java

---
+++ chap07/src/sec01/exam07/pack2/D.java
@@ -0,0 +1,14 @@
+package sec01.exam07.pack2;
+
+import sec01.exam07.pack1.A;
+
+public class D extends A {
+
+    // 다른 패키지지만 A 클래스를 상속받아 protected 요소에 접근할 수 있음.
+    public D() {
+        super();
+        this.field = "value";
+        this.method();
+    }
+
+}
