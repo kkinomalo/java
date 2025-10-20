fde8b3af00f3f326a6f6ef89b62ea25279713071
Nohjiwan <nojiwan97@gmail.com>
2025-10-20 15:26:35 +0900

    Update BankAccountExample.java

--- cote/src/exam1020/BankAccountExample.java
+++ cote/src/exam1020/BankAccountExample.java
@@ -4,6 +4,9 @@ public class BankAccountExample {
 
 	public static void main(String[] args) {
 		BankAccount account = new BankAccount("김자바", "111-1111-2233");
+		BankAccount account2 = new BankAccount("이자바", "111-1132-2123");
+		
+		System.out.println(BankAccount.getCount());
 		int currentBalance = account.deposit(10000);
 		System.out.println(currentBalance);
 		System.out.println(account.getBalance());
