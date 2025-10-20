20b5cd9171450f80f52a8f718f9a656a177589cc
Nohjiwan <nojiwan97@gmail.com>
2025-10-20 15:14:33 +0900

    Create BankAccount.java

---
+++ cote/src/exam1020/BankAccount.java
@@ -0,0 +1,46 @@
+package exam1020;
+
+public class BankAccount {
+	private String name;
+	private String accountNumber;
+	private int balance;
+	
+	public BankAccount(String name, String accountNumber) {
+		this.name = name;
+		this.accountNumber = accountNumber;
+		this.balance = 0;
+	}
+	public BankAccount(String name, String accountNumber, int balance) {
+		this.name = name;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public int withdrawal(int amount) {
+		int finalAmount = amount > balance? balance:amount;
+		
+		balance -= finalAmount;
+		return finalAmount;
+	}
+	public int deposit(int amount) {
+		balance += amount;
+		return balance;
+	}
+}
