ce69489f42baf3df55a61c05f16a6848302bccfb
Nohjiwan <nojiwan97@gmail.com>
2025-10-22 14:46:25 +0900

    Create PartTimeEmployee.java

---
+++ cote/src/exam1020/PartTimeEmployee.java
@@ -0,0 +1,11 @@
+package exam1020;
+
+public class PartTimeEmployee extends Employee{
+	private int hourRate;
+	private int workHour;
+	
+	public PartTimeEmployee(String name, String employeeId, int houeRate, int workHours) {
+		super(name, employeeId);
+		 
+	}
+}
