a20f380f0638fb56e168fff8968574ad28b1feb4
Nohjiwan <nojiwan97@gmail.com>
2025-10-22 14:46:21 +0900

    Create FullTimeEmployee.java

---
+++ cote/src/exam1020/FullTimeEmployee.java
@@ -0,0 +1,20 @@
+package exam1020;
+
+public class FullTimeEmployee extends Employee{
+	private int annualSalary;
+	public FullTimeEmployee(String name, String employeeId, int annualSalary) {
+		super(name, employeeId);
+		this.annualSalary = annualSalary;
+	}
+	public int getAnnualSalary() {
+		return annualSalary;
+	}
+	public void setAnnualSalary(int annualSalary) {
+		this.annualSalary = annualSalary;
+	}
+	
+	@Override
+	public int calculatepay() {
+		return annualSalary/12;
+	}
+}
